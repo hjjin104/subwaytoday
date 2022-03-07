493b6994facfe21c574cba4b1eed4f96ac1a85e9
chloe <nyear0510@gmail.com>
2022-03-08 08:34:34 +0900

    app.py 수정

--- app.py
+++ app.py
@@ -3,21 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-   return render_template('Home.html')
+# @app.route('/')
+# def home():
+#    return render_template('Home.html')
 
 @app.route('/menu')
 def menu():
-    return render_template('Menu.html')
+    return render_template('menu.html')
 
-# @app.route('/menu')
-# def menu():
-#     return render_template('Menu.html')
-#
-# @app.route('/menu')
-# def menu():
-#     return render_template('Menu.html')
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
